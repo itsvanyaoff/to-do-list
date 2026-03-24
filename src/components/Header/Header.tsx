d86a47e82ad5e8369c22ec51ad52254b56b64e98
itsvantaoff <itsvanyaoff@yandex.ru>
2026-03-24 18:51:27 +0300

    выполнен 2 урок

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import classes from './Header.module.scss';
+
+export const Header = () => {
+    return (
+        <>
+            <header className={classes.header}>
+                <div className={classes.container}>
+                    <a href="/" className={classes.link}>ToDo</a>
+                    {/* <a href="/" className={`${classes.link} ${classes.active}`}>ToDo</a> */}
+                </div>
+            </header>
+        </>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
-import {ToDoList} from './pages/ToDoList';
-
+// import './index.scss';
+import './assets/scss/normalize.scss';
+import './assets/scss/style.scss';
+import { ToDoListPage } from './pages/ToDoListPage';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ToDoList />
+    <ToDoListPage />
   </React.StrictMode>
 );
 
